b8e8c003a507230da195704b77df2b37cf52ebfd
Julian Nicholls <juliannicholls29@gmail.com>
2017-08-16 22:12:44 +0100

    Change survey fields to be horizontal

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -4,10 +4,12 @@ import React   from 'react';
 
 export default ({ input, label, placeholder, meta: { touched, error } }) => {
   return (
-    <div>
-      <label>{label}</label>
-      <input type="text" {...input} placeholder={placeholder}/>
-      {touched && <div className="red-text survey-error">{error}</div>}
+    <div className="form-group row">
+      <label className="col-sm-2 col-form-label">{label}</label>
+      <div className="col-sm-10">
+        <input type="text" className="form-control" {...input} placeholder={placeholder}/>
+        {touched && <div className="text-danger ml-2">{error}</div>}
+      </div>
     </div>
   )
 }
